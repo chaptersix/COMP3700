b3220f8a2d12a86e1f850ada08dc46a4c5c59559
tfr00 <tfr0001@auburn.edu>
2019-04-30 15:23:29 -0500

    An additional frame for the AccountSelectionMenu, much like the different pages set up in the TigerWallet for loop!

--- AccountSelectionMenu.py
+++ AccountSelectionMenu.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+import CheckingAccountsMenu as cam
+import SavingsAccountsMenu as sam
 
 
 # This class helps to the user to narrow down which account he or she would like to view.
@@ -12,27 +14,42 @@ class AccountSelectionMenu(tk.Frame):
 
         self.controller = controller
 
+        instructions_frame = tk.Frame(self)
+        instructions_frame.pack()
+
+        # Add text for instructions.
+        instructions = tk.Label(instructions_frame, text="Select which accounts you would like to view", fg="black")
+        instructions.pack()
+
+        # Initialize other sub-pages of the AccountSelectionMenu
+        # tuple containing all active pages
+        page_list = (cam.ChecckingAccountsMenu, sam.SavingsAccountsMenu)
+        # dictionary mapping frames to the corresponding page names
+        self.frames = {}
+        # add additional pages to this tuple
+        for F in page_list:
+            page_name = F.__name__
+            frame = F(context=context, controller=self)
+            self.frames[page_name] = frame
+            # ensures frames are placed in the same location
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        # Add options for the user's navigation.
         options_frame = tk.Frame(self)
         options_frame.pack()
 
-        # Add text for instructions.
-        instructions = tk.Label(options_frame, text="Select which accounts you would like to view", fg="black")
-        instructions.pack()
-
         checking_button = tk.Button(options_frame, text="Checking Accounts", fg="black")
-        checking_button.pack(side="left")
+        checking_button.grid(row = 0, column = 0)
+
         savings_button = tk.Button(options_frame, text="Savings Accounts", fg="black")
-        savings_button.pack(side="left")
+        savings_button.grid(row = 0, column = 1)
 
-        # Make a frame for the home button and for the exit button.
+        home_button = tk.Button(options_frame, text="Back Home", fg="black",
+                                command=lambda: controller.show_frame("HomePage"))
+        home_button.grid(row = 1, columnspan = 2)
 
-        bottom_frame = tk.Frame(self)
-        bottom_frame.pack(side="bottom")
-        home_button = tk.Button(bottom_frame, text="Back Home", fg="black")
-        home_button.pack()
-
-        exit_button = tk.Button(bottom_frame, text="Exit", fg="black", command = quit)
-        exit_button.pack()
+        exit_button = tk.Button(options_frame, text="Exit", fg="black", command = quit)
+        exit_button.grid(row = 3, columnspan = 2)
 
 
 
