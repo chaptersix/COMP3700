2bf8a1e3820cba64fc28473fed36d325bcbcb5c6
dgw0015 <44581375+dgw0015@users.noreply.github.com>
2019-05-02 00:26:56 -0500

    Delete BillReminder.py

--- BillReminder.py
+++
@@ -1,9 +0,0 @@
-class BillReminders():
-    def __init__(self, name, notify_date, exp_date):
-        self.name = name
-        self.notification_date = notify_date
-        self.exp_date = exp_date
-
-    # in order for this to be persistent across sessions and users, 
-    # goals should be written to a file associated with a username    
-    bill_reminders_list = []
